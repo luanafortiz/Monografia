c358f066e62e2fcfd0f86a47c8aff9a97a7cbbf9
luanafortiz <luanadfortiz@gmail.com>
2021-09-01 11:18:22 -0300

    abri o arquivo da PNADC

--- code/build.R
+++ code/build.R
@@ -1,1 +1,10 @@
 #inicio do projeto de monografia
+
+library(tidyverse)
+install.packages("PNADcIBGE")
+library(PNADcIBGE)
+
+setwd("C:\\Users\\luana\\OneDrive\\Área de Trabalho\\PUC\\Monografia\\PNADC")
+microdata_file <- file.path("PNADC_012012.txt")
+chave_file <- file.path("input_PNADC_trimestral.txt")
+xxx <- PNADcIBGE::read_pnadc(microdata = microdata_file, input_txt = chave_file)
