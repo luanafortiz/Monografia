fa7831300fc1ecd4500514617294494fec321042
luanafortiz <luanadfortiz@gmail.com>
2021-09-22 02:36:05 -0300

    primeira matriz

--- code/build-painel.R
+++ code/build-painel.R
@@ -2,22 +2,17 @@ library(tidyverse)
 library(haven)
 library(dplyr)
 
-# teste
-painel1 <- read_dta(file = "PNAD_painel_1_rs.dta")
-
-painel1 <- painel1 %>% group_by(idind) %>% arrange(Ano, Trimestre)
-
-painel1 <- painel1 %>% filter(UF==33) %>% group_by(idind) %>% arrange(Ano, Trimestre)
 
 
 # painel 7
 
-painel7 = read_dta(file = "input/PNAD_painel_7_rs.dta")
+painel7 = read_dta(file = "input/PNAD_painel_7_rs.dta") %>% slice_head(prop = 0.1)
 
 variaveis_escolhidas = c("Ano", "Trimestre", "UF", "Capital", 
                          "V1008", # número de seleção do domicilio
                          "V1014", # grupo de amostra
                          "V1022", # urbana ou rural
+                         "V1028", # peso com pos estratificação 
                          "VD2002", # condição no domicílio (pai, mae, avó..)
                          "V2007", # sexo
                          "V2009", # idade
@@ -53,12 +48,21 @@ variaveis_escolhidas = c("Ano", "Trimestre", "UF", "Capital",
 # selecionando as variáveis importantes
 painel7 = painel7 %>% select(all_of(variaveis_escolhidas))
 
-# filtrar mulheres em trimestre 1 2019
-m_1tri19 = painel7 %>% dplyr::filter(V2007 == 2 & Ano == 2019 & Trimestre == 1) 
+# filtrar trimestres desejados (1 e 2 2019)
+trimestres = painel7 %>% dplyr::filter(Ano == 2019 & (Trimestre == 1 | Trimestre == 2)) 
 
-    # só mulheres ocupadas 1º tri 2019
-    m_1tri19_ocupadas = m_1tri19 %>% mutate(ocupadas = ifelse(VD4002 == 1, 1, 0)) %>% 
-      ungroup() %>% summarise(sum(ocupadas, na.rm = T)) %>% as.integer()
+    # matriz para mulheres
+
+          # ocupada x ocupada 
+          uu = trimestres %>% group_by(idind) %>% arrange(idind, Ano, Trimestre) %>% 
+            mutate(xxx = ifelse(VD4002 == 1 & Trimestre == 1, 1, 0),                # se estiver ocupada no 1 tri
+                   xxx2 = ifelse(VD4002 == 1 & Trimestre == 2, 1, 0),
+                   zzz = ifelse(VD4002 == 1 & Trimestre == 2 & xxx2 == 1, 1, 0),    # se ela continuou ocupada
+                   kkk = lag(V1028)) %>%                                           # pega o peso dessa pessoa no periodo 1
+            ungroup() %>% 
+            summarise(uu = ((sum(zzz * kkk, na.rm = T))/(sum(xxx * V1028, na.rm = T)))) 
+    
+    
 
     # dessas mulheres, pretas 1º tri 2019
     mp_1tri19_ocupadas = m_1tri19 %>% mutate(ocupadas_p = ifelse(VD4002 == 1 & V2010 == 2, 1, 0)) %>% 
@@ -77,3 +81,5 @@ h_1tri19 = painel7 %>% dplyr::filter(V2007 == 1 & Ano == 2019 & Trimestre == 1)
     # só homens ocupados 1º tri 2019
     h_1tri19_ocupadas = h_1tri19 %>% mutate(h_ocupadas = ifelse(VD4002 == 1, 1, 0)) %>% 
       ungroup() %>% summarise(sum(h_ocupadas, na.rm = T)) %>% as.integer()
+    
+    
